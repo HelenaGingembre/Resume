822232dc0ee18775e6de65297a5ba29c52710d40
Helena Gingembre <e.izotovagingembre@outlook.com>
2024-05-13 19:44:34 +0200

    update

---
+++ src/documentPDF.js
@@ -0,0 +1,15 @@
+import { Document } from '@react-pdf/renderer';
+import App from './App';
+import { PDFViewer } from '@react-pdf/renderer';
+
+function MyDocumentPDF() {
+  return (
+    // <PDFViewer>
+    <Document author="Olena Gingembre" title="CV">
+      <App size="A4" />
+    </Document>
+    // </PDFViewer>
+  );
+}
+
+export default MyDocumentPDF;
